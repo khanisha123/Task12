5fd4f034385af9e472257d372eb2743fe9a5ca8e
khanisha123 <khanisha@code.edu.az>
2021-10-14 05:31:24 +0400

    Add project files.

---
+++ Task12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Task.Counter = 0;
+            String word1 = "Salam Kamran. Necəsən Kamran";
+            
+            
+
+            foreach (var item in word1)
+            {
+                if (item == "Kamran")
+                {
+                    Task.Counter++;
+                }
+            }
+            
+            Console.WriteLine(Task.Counter);
+        }
+    }
+}
---
+++ Task12/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task12
+{
+    static class Task
+
+    {
+        public static int Counter;
+        
+        
+        
+        public static string  ToString(this string word1) 
+        {
+
+
+            return word1;
+
+        }
+        
+        
+    }
+}
